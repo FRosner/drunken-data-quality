c4e16b7f1a0124d5ce9c5ff11cd7c42c4d3af7eb
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2015-10-16 18:20:11 +0200

    method and class annotations added according to changed API

--- src/main/scala/de/frosner/ddq/reporters/MarkdownReporter.scala
+++ src/main/scala/de/frosner/ddq/reporters/MarkdownReporter.scala
@@ -4,7 +4,17 @@ import java.io.PrintStream
 
 import de.frosner.ddq._
 
+/**
+ * A class which produces a markdown report of [[de.frosner.ddq.CheckResult]].
+ *
+ * @param stream The [[java.io.PrintStream]] to put the output
+ **/
 case class MarkdownReporter(stream: PrintStream) extends PrintStreamReporter {
+
+  /**
+   * Output markdown report of a given checkResult to the stream passed to the constructor
+   * @param checkResult The [[de.frosner.ddq.CheckResult]] to be reported
+   */
   override def report(checkResult: CheckResult): Unit = {
     stream.println(s"# ${checkResult.header}\n")
     stream.println(s"${checkResult.prologue}\n")
