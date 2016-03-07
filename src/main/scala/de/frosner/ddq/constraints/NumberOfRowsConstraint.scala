17be4ec3f9dfa2c3de88e7a5c19b21e4a9841b70
Frank Rosner <frank@fam-rosner.de>
2016-03-07 17:00:18 +0100

    #76 more general num rows constraint (with arbitrary column expression)

--- src/main/scala/de/frosner/ddq/constraints/NumberOfRowsConstraint.scala
+++ src/main/scala/de/frosner/ddq/constraints/NumberOfRowsConstraint.scala
@@ -1,16 +1,29 @@
 package de.frosner.ddq.constraints
 
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{Column, DataFrame}
+import org.apache.spark.sql.functions.{count, lit}
 
-case class NumberOfRowsConstraint(expected: Long) extends Constraint {
+case class NumberOfRowsConstraint private[ddq] (expected: Column) extends Constraint {
 
   val fun = (df: DataFrame) => {
-    val count = df.count
+    val countDf = df.agg(count(new Column("*")).as(NumberOfRowsConstraint.countKey))
+    val actual = countDf.collect().map(_.getLong(0)).apply(0)
+    val satisfied = countDf.select(expected).collect().map(_.getBoolean(0)).apply(0)
     NumberOfRowsConstraintResult(
       constraint = this,
-      actual = count,
-      status = if (count == expected) ConstraintSuccess else ConstraintFailure
+      actual = actual,
+      status = if (satisfied) ConstraintSuccess else ConstraintFailure
     )
+  }
+
+}
+
+object NumberOfRowsConstraint {
+
+  private[constraints] val countKey: String = "count"
+
+  def apply(expected: Column => Column): NumberOfRowsConstraint = {
+    new NumberOfRowsConstraint(expected(new Column(countKey)))
   }
 
 }
