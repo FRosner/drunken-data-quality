1deca2aea170cf517418159093bce99aae26a8c2
Frank Rosner <frank@fam-rosner.de>
2016-03-14 00:08:57 +0100

    #73 fix first prototype for AlwaysNullConstraint

--- src/main/scala/de/frosner/ddq/constraints/AlwaysNullConstraintResult.scala
+++ src/main/scala/de/frosner/ddq/constraints/AlwaysNullConstraintResult.scala
@@ -11,9 +11,9 @@ case class AlwaysNullConstraintResult(constraint: AlwaysNullConstraint,
     val maybePluralS = maybeNonNullRows.map(n => if (n == 1) "" else "s")
     (status, maybeNonNullRows, maybePluralS) match {
       case (ConstraintError(throwable), None, None) =>
-        s"Checking $columnName for being always null failed: $throwable"
+        s"Checking column $columnName for being always null failed: $throwable"
       case (ConstraintSuccess, _, Some(pluralS)) =>
-        s"Column $pluralS is always null."
+        s"Column $columnName is always null."
       case (ConstraintFailure, Some(nonNullRows), Some(pluralS)) =>
         s"Column $columnName contains $nonNullRows non-null row$pluralS (should always be null)."
     }
