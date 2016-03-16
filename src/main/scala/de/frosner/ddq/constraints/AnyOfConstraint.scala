93d4e35fd738033798424a9ab98e770e18283ab4
Frank Rosner <frank@fam-rosner.de>
2016-03-16 08:31:34 +0100

    #73 remove unused import

--- src/main/scala/de/frosner/ddq/constraints/AnyOfConstraint.scala
+++ src/main/scala/de/frosner/ddq/constraints/AnyOfConstraint.scala
@@ -1,8 +1,5 @@
 package de.frosner.ddq.constraints
 
-import java.text.SimpleDateFormat
-
-import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{Column, DataFrame}
 
 import scala.util.Try
