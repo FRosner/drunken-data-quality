3f23870ed9cc4585dbaa2ba442cd9c7e46a840cd
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2016-06-17 16:07:23 +0200

    replace dummy reporter with markdown reporter in pyddq integration tests

--- python/tests/integration/test_constraints.py
+++ python/tests/integration/test_constraints.py
@@ -4,15 +4,16 @@ from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.sql import types as t
 
-from utils import DummyReporter
 from pyddq.core import Check
+from pyddq.reporters import MarkdownReporter
+from pyddq.streams import ByteArrayOutputStream
 
 
 class ConstraintTest(unittest.TestCase):
     def setUp(self):
         self.sc = SparkContext()
         self.sqlContext = SQLContext(self.sc)
-        self.reporter = DummyReporter()
+        self.reporter = MarkdownReporter(ByteArrayOutputStream())
 
     def tearDown(self):
         self.sc.stop()
@@ -29,7 +30,10 @@ It has a total number of 2 columns and 3 rows.
 - *FAILURE*: Column _1 is not a key (1 non-unique tuple).
 - *SUCCESS*: Columns _1, _2 are a key.
 """.strip()
-        self.assertEqual(self.reporter.get_output(), expected_output)
+        self.assertEqual(
+            self.reporter.output_stream.get_output(),
+            expected_output
+        )
 
     def test_isNeverNull(self):
         df = self.sqlContext.createDataFrame([(1, "a"), (1, None), (3, "c")])
@@ -43,7 +47,10 @@ It has a total number of 2 columns and 3 rows.
 - *SUCCESS*: Column _1 is never null.
 - *FAILURE*: Column _2 contains 1 row that is null (should never be null).
 """.strip()
-        self.assertEqual(self.reporter.get_output(), expected_output)
+        self.assertEqual(
+            self.reporter.output_stream.get_output(),
+            expected_output
+        )
 
     def test_isAlwaysNull(self):
         schema = t.StructType([
@@ -64,7 +71,10 @@ It has a total number of 2 columns and 3 rows.
 - *FAILURE*: Column _1 contains 3 non-null rows (should always be null).
 - *SUCCESS*: Column _2 is always null.
 """.strip()
-        self.assertEqual(self.reporter.get_output(), expected_output)
+        self.assertEqual(
+            self.reporter.output_stream.get_output(),
+            expected_output
+        )
 
     def test_isConvertibleTo(self):
         df = self.sqlContext.createDataFrame([(1, "a"), (1, None), (3, "c")])
@@ -80,7 +90,10 @@ It has a total number of 2 columns and 3 rows.
 - *SUCCESS*: Column _1 can be converted from LongType to IntegerType.
 - *ERROR*: Checking whether column _1 can be converted to ArrayType(IntegerType,true) failed: org.apache.spark.sql.AnalysisException: cannot resolve 'cast(_1 as array<int>)' due to data type mismatch: cannot cast LongType to ArrayType(IntegerType,true);
 """.strip()
-        self.assertEqual(self.reporter.get_output(), expected_output)
+        self.assertEqual(
+            self.reporter.output_stream.get_output(),
+            expected_output
+        )
 
     def test_isFormattedAsDate(self):
         df = self.sqlContext.createDataFrame([
@@ -97,6 +110,10 @@ It has a total number of 1 columns and 3 rows.
 
 - *SUCCESS*: Column _1 is formatted by yyyy-MM-dd HH:mm:ss.
 """.strip()
+        self.assertEqual(
+            self.reporter.output_stream.get_output(),
+            expected_output
+        )
 
     def test_isAnyOf(self):
         df = self.sqlContext.createDataFrame([(1, "a"), (2, "b"), (3, "c")])
@@ -110,7 +127,10 @@ It has a total number of 2 columns and 3 rows.
 - *FAILURE*: Column _1 contains 1 row that is not in Set(1, 2).
 - *SUCCESS*: Column _2 contains only values in Set(a, b, c).
 """.strip()
-        self.assertEqual(self.reporter.get_output(), expected_output)
+        self.assertEqual(
+            self.reporter.output_stream.get_output(),
+            expected_output
+        )
 
     def test_isMatchingRegex(self):
         df = self.sqlContext.createDataFrame([
@@ -131,7 +151,10 @@ It has a total number of 2 columns and 3 rows.
 - *SUCCESS*: Column _1 matches ^Hello
 - *FAILURE*: Column _2 contains 1 row that does not match world$
 """.strip()
-        self.assertEqual(self.reporter.get_output(), expected_output)
+        self.assertEqual(
+            self.reporter.output_stream.get_output(),
+            expected_output
+        )
 
     def test_hasFunctionalDependency(self):
         df = self.sqlContext.createDataFrame([
@@ -148,7 +171,10 @@ It has a total number of 4 columns and 3 rows.
 
 - *SUCCESS*: Column _3 is functionally dependent on _1, _2.
 """.strip()
-        self.assertEqual(self.reporter.get_output(), expected_output)
+        self.assertEqual(
+            self.reporter.output_stream.get_output(),
+            expected_output
+        )
 
     def test_hasForeignKey(self):
         base = self.sqlContext.createDataFrame([
@@ -168,7 +194,10 @@ It has a total number of 3 columns and 3 rows.
 
 - *SUCCESS*: Columns _1->_1, _2->_2 define a foreign key pointing to the reference table [_1: bigint, _2: bigint, _3: bigint].
 """.strip()
-        self.assertEqual(self.reporter.get_output(), expected_output)
+        self.assertEqual(
+            self.reporter.output_stream.get_output(),
+            expected_output
+        )
 
     def test_isJoinableWith(self):
         base = self.sqlContext.createDataFrame([
@@ -188,7 +217,10 @@ It has a total number of 3 columns and 3 rows.
 
 - *SUCCESS*: Key _1->_1, _2->_2 can be used for joining. Join columns cardinality in base table: 2. Join columns cardinality after joining: 2 (100.00%).
 """.strip()
-        self.assertEqual(self.reporter.get_output(), expected_output)
+        self.assertEqual(
+            self.reporter.output_stream.get_output(),
+            expected_output
+        )
 
     def test_satisfies(self):
         df = self.sqlContext.createDataFrame([
@@ -204,7 +236,10 @@ It has a total number of 2 columns and 3 rows.
 - *SUCCESS*: Constraint _1 > 0 is satisfied.
 - *SUCCESS*: Constraint _2 = 'a' is satisfied.
 """.strip()
-        self.assertEqual(self.reporter.get_output(), expected_output)
+        self.assertEqual(
+            self.reporter.output_stream.get_output(),
+            expected_output
+        )
 
 
 if __name__ == '__main__':
