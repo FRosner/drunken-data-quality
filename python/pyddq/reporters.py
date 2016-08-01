05e79ee4acf849c0470ea1cd5669642bf128daf4
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2016-08-01 11:29:10 +0200

    #105 pyddq.repoters.ZeppelinReporter is implemented

--- python/pyddq/reporters.py
+++ python/pyddq/reporters.py
@@ -1,5 +1,6 @@
 import sys
 from pyddq.streams import PrintStream, OutputStream, FileOutputStream
+from py4j.java_gateway import get_field
 
 
 class Reporter(object):
@@ -38,4 +39,24 @@ class ConsoleReporter(Reporter):
         print_stream = PrintStream(jvm, self.output_stream)
         return jvm.de.frosner.ddq.reporters.ConsoleReporter(
             print_stream.jvm_obj
+        )
+
+
+class ZeppelinReporter(Reporter):
+    """
+    A class which produces a report of core.Check.run in a Zeppelin notebook note.
+    Args:
+        zeppelin_context (PyZeppelinContext): It is defined as "z" in a Zeppelin
+            notebook.
+    """
+    def __init__(self, zeppelin_context):
+        self.zeppelin_context = zeppelin_context
+
+    def get_jvm_reporter(self, jvm):
+        jvm_zeppelin_context = self.zeppelin_context.z
+        jvm_intp_context = jvm_zeppelin_context.getInterpreterContext()
+        jvm_output_stream = get_field(jvm_intp_context, "out")
+        jvm_print_stream = jvm.java.io.PrintStream(jvm_output_stream)
+        return jvm.de.frosner.ddq.reporters.ZeppelinReporter(
+            jvm_print_stream
         )
